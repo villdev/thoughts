e2a04e9d409269465055293b5837581304c209b6
vill <villdevelop@gmail.com>
2021-09-22 09:42:10 +0530

    fix: updated backend url and added netlify toml file

--- client/src/utils/apiEndpoints.js
+++ client/src/utils/apiEndpoints.js
@@ -1,6 +1,7 @@
-export const BASE_URL = 'http://localhost:2000';
-export const CREATE_NOTE = '/api/note';
-export const UPDATE_NOTE = '/api/note/';
-export const DELETE_NOTE = '/api/note/';
-export const GET_ALL_NOTES = '/api/notes/all';
-export const GET_TRASH_NOTES = '/api/notes/trash';
+// export const BASE_URL = 'http://localhost:2000';
+export const BASE_URL = "https://thoughts-backend.herokuapp.com/";
+export const CREATE_NOTE = "/api/note";
+export const UPDATE_NOTE = "/api/note/";
+export const DELETE_NOTE = "/api/note/";
+export const GET_ALL_NOTES = "/api/notes/all";
+export const GET_TRASH_NOTES = "/api/notes/trash";
